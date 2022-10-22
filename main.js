29feb6c5a70fb3d068f7c1e0095e93c7ffdb5030
Tofu <91769994+Awesome-Tofu@users.noreply.github.com>
2022-10-22 22:35:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,27 +11,19 @@ client.on('messageCreate', (message) => {
     if (message.author.id === client.user.id) return
     message.markSeen();
 
-    if(message.content.toLowerCase().includes('/meme')){
-    chatbot(`https://meme-api.herokuapp.com/gimme/wholesomememes`)
-    .then(res => res.json())
-    .then(json => {
-      message.chat.sendPhoto(json.url);
-    }).catch(err => {});
-}
-    else
-        console.log(message.content)
-    });
-
-client.on('messageCreate', (message) => {
-    if (message.author.id === client.user.id) return
-    message.markSeen();
-
     if(message.content.toLowerCase().includes('owner')){ 
         return message.chat.sendMessage(`Calling my owner for u ... ${god} #Kidding`);
     }
     else if(message.content.toLowerCase().includes('tofu')){ 
         return message.chat.sendMessage(`OwO, how do you know my master ${god}?`);
           } 
+    else if(message.content.toLowerCase().includes('/meme')){
+    chatbot(`https://meme-api.herokuapp.com/gimme/wholesomememes`)
+    .then(res => res.json())
+    .then(json => {
+      message.chat.sendPhoto(json.url);
+    }).catch(err => {});
+}
     
   else
     chatbot(`http://api.brainshop.ai/get?bid=169976&key=vroVmiAsTNWUy5rZ&uid=[uid]&msg=${encodeURIComponent(message.content)}`) //Replace <Enter Brainshop API> to API
