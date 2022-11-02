d67f052c884f74903186cf92242fb80e6714b15f
Tofu <91769994+Awesome-Tofu@users.noreply.github.com>
2022-11-02 16:01:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -49,8 +49,8 @@ client.on('messageCreate', (message) => {
 }
     
   else
-    //chatbot(`http://api.brainshop.ai/get?bid=169976&key=vroVmiAsTNWUy5rZ&uid=[uid]&msg=${encodeURIComponent(message.content)}`)Replace <Enter Brainshop API> to API
-    chatbot(`https://api.bakufu.tech/api/chatbot/cleverbot?name=Kurumi&owner=%40aditya.agatsuma&message=${encodeURIComponent(message.content)}`)
+    chatbot(`http://api.brainshop.ai/get?bid=169976&key=vroVmiAsTNWUy5rZ&uid=[uid]&msg=${encodeURIComponent(message.content)}`)Replace <Enter Brainshop API> to API
+    //chatbot(`https://api.bakufu.tech/api/chatbot/cleverbot?name=Kurumi&owner=%40aditya.agatsuma&message=${encodeURIComponent(message.content)}`)
     .then(res => res.json())
     .then(json => {
       message.chat.sendMessage(json.reply);
